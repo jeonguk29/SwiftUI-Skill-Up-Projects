584de1af69cdb64053e0ee6c02bae4aa47f5d338
jeonguk29 <jeonguk29@naver.com>
2024-05-26 16:34:06 +0900

    메모리 부족 현상과Crash의 차이 및 메모리 부족 현상 catch 방법
    AppDelegate에서 메모리 체크 방법
    applicationDidReceiveMemoryWarning() 사용
    
    ViewController에서 메모리 체크하는 방법
     override func didReceiveMemoryWarning() {
            super.didReceiveMemoryWarning()
    
            print("home vc memory warning")
        }
    
    메모리 체크 방법 3 이렇게 구독해서 원하는 클래스에서 확인 가능
    
     /*
             유의 사항
             MemoryWarning을 캐치하는 곳이 원인이 아닐 수 있음 이곳으로 정보를 전달해 주기 위함임이 코드로 인해 즉 해당 영역에서 발생한 게 아닐 수 있다는 걸 알고 있어야 함 하지만 MemoryWarning 발생 시 메모리를 확보하기 위해서 해당 부분에 적절한 작업을 해줄 수도 있고 로그를 넣어 줄 수도 있음
             */
            NotificationCenter.default.publisher(for: UIApplication.didReceiveMemoryWarningNotification)
                .sink { [weak self] _ in
                    print("캐시 clear")
                    self?.clear()
                }
                .store(in: &self.cancellables)

---
+++ 부록1_02MemoryDebug/MemoryDebug/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  MemoryDebug
+//
+//  Created by Lecture on 2023/12/04.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    
+    // AppDelegate에서 메모리 체크 방법 앱이 시스템으로부터 메모리 경고를 수신하면 대리자에게 알립니다.
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        print("memory warning")
+    }
+
+
+}
+
---
+++ 부록1_02MemoryDebug/MemoryDebug/Home/ViewController.swift
@@ -0,0 +1,141 @@
+//
+//  ViewController.swift
+//  MemoryDebug
+//
+//  Created by Lecture on 2023/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    enum Row: Int, CaseIterable {
+        case crash
+        case memoryType1
+        case memoryType2
+        case memoryType3
+        case cacheTest
+        case weak
+        case unowned
+        case unownedOptional
+    }
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.register(ListCell.self, forCellReuseIdentifier: "cell")
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.rowHeight = 50
+        
+        TaskSample().doSomething()
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Row.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = self.cellTitle(indexPath: indexPath)
+        cell.contentConfiguration = configuration
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let row = Row(rawValue: indexPath.row) else {
+            return
+        }
+        
+        switch row {
+        case .crash:
+            _ = [Int]()[1]
+        case .memoryType1:
+            let aModule = AModule()
+            _ = BModule(aModule: aModule)
+            
+            let circularA = CircularA()
+            let circularB = CircularB()
+            let circularC = CircularC()
+            circularA.b = circularB
+            circularB.c = circularC
+            circularC.a = circularA
+        case .memoryType2:
+            let vc = MemoryWarningType2ViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
+        case .memoryType3:
+            let vc = MemoryWarningType3ViewController()
+            self.navigationController?.pushViewController(vc, animated: true)
+        case .cacheTest:
+            SampleCache.shared.add(data: DummyGenerator.make())
+        case .weak:
+            let main = WeakMain()
+            let sub = WeakSub()
+            main.sub = sub
+            Task {
+                try? await Task.sleep(nanoseconds: 1_000_000_000)
+                print(String(describing: main.sub?.value))
+            }
+        case .unowned:
+            let sub = UnownedSub()
+            let main = UnownedMain(sub: sub)
+            Task {
+                try? await Task.sleep(nanoseconds: 1_000_000_000)
+                print(String(describing: main.sub.value))
+            }
+        case .unownedOptional:
+            let main = UnownedOptionalMain()
+            let sub = UnownedOptionalSub()
+            main.sub = sub
+            Task {
+                try? await Task.sleep(nanoseconds: 1_000_000_000)
+                print(String(describing: main.sub?.value))
+            }
+        }
+    }
+    
+    private func cellTitle(indexPath: IndexPath) -> String? {
+        guard let row = Row(rawValue: indexPath.row) else {
+            return nil
+        }
+        
+        switch row {
+        case .crash:
+            return "크래시 발생"
+        case .memoryType1:
+            return "메모리 문제 발생 유형 1"
+        case .memoryType2:
+            return "메모리 문제 발생 유형 2"
+        case .memoryType3:
+            return "메모리 문제 발생 유형 3"
+        case .cacheTest:
+            return "MemoryWarning을 활용한 데이터정리"
+        case .weak:
+            return "Weak Reference 접근 테스트"
+        case .unowned:
+            return "Unowned Reference 접근 테스트"
+        case .unownedOptional:
+            return "Unowned Optional Reference 접근 테스트"
+        }
+    }
+    
+    private func printCancel() {
+        print("canceled")
+    }
+    
+    // ViewController에서 메모리 체크하는 방법
+    /*
+     ViewController는 class즉 참조 타입이라서 참조 사이클이 생겨서 해제되지 않는다면 아래 didReceiveMemoryWarning 호출시 예를 들면 똑같은 뷰컨 5개가 로드되지 않았다면 동시에 5개의 로그가 들어올 것임
+     스유는 벨류 타입이라 그런 상황이 발생하지는 않겠지만 필요에 따라 유킷에서는 해당 코드 잘 넣어두면 대응할 때 굉장히 유용할 것임
+     */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+        print("home vc memory warning")
+    }
+
+}
+
